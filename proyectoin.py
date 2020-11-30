e68506e49ec85171c51d60393c2af9bdee9c1215
DiegoSanabria1985 <73298640+DiegoSanabria1985@users.noreply.github.com>
2020-11-29 20:25:33 -0500

    ultima version

--- proyectoin.py
+++ proyectoin.py
@@ -1,11 +1,18 @@
 import tweepy
 import json
+import boto3
+import os
+
 
 # 4 cadenas para la autenticacion
 consumer_key = "Wcol0aOJbVkYOqsYkA57pqO64"
 consumer_secret = "DZ43ZrpdV8VS6GdO0LSaoacfOFciIrRg92Bl6QpktaQE4nd5qN"
 access_token = "161715257-t2hUBSkhtbkoFNARy4jtaETzAOHQ60Xx1VxJW0eS"
 access_token_secret = "lAaKqYJUNDkVAVlbbZgVgkC3T89LcH9lnmV8F22e6sT0j"
+
+
+clientes3 = boto3.cliente('s3', aws_access_key_id="AKIAIRC3YFKTECX6J7KQ",
+                      aws_secret_access_key="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")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -18,6 +25,12 @@ api = tweepy.API(auth,
 data = api.me()
 print(json.dumps(data._json, indent=4))
 
+#carpeta = 'C:/Users/DELL/proyectoIN/proyecto'
+#archivo = "tweet.json"
+
+#with open(os.path.join(carpeta, archivo), 'w') as file:
+#   json.dump(data,file)
+
 #data_json = data._json
 #print(data_json)
 
@@ -29,8 +42,13 @@ print(json.dumps(data._json, indent=4))
 #data = api.followers(screen_name="nike")
 #print(len(data))
 # Explicar cursor
-for user in tweepy.Cursor(api.followers, screen_name="nike").items(15):
+for user in tweepy.Cursor(api.followers, screen_name="nike").items(10):
    print(json.dumps(user._json, indent=4))
+
+#archivo1 = "tweet.json"
+
+#with open(os.path.join(carpeta, archivo1), 'w') as file:
+#   json.dump(user, file)
 
 #Obtener followees
 #for user in tweepy.Cursor(api.friends, screen_name="nike").items(2):
@@ -46,7 +64,23 @@ for tweet in tweepy.Cursor(api.search, q="maradona", tweet_mode="extended").item
    print(json.dumps(tweet._json, indent=4));
 
 
+#archivo2 = "tweet.json"
+
+#with open(os.path.join(carpeta, archivo2), 'w') as file:
+#   json.dump(tweet, file)
 
 
+#clientes3.put_object(
+#    ACL='authenticated-read',
+ #   Body=carpeta+archivo2,
+   # Bucket='uniquindioia',
+   # Key= "/proyecto" + archivo2,
+#)
+
+clientes3.put_object(
+        Body=str(json.dumps(tweet, user, data)),
+        Bucket='uniquindioia',
+        Key='proyecto/' + 'archivos.json'
+    )
 
 
